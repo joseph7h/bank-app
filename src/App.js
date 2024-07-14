31e1d1c7de72d3e0d3ac8f69d7bbf96f35988b4c
Jun An <junan@Juns-MacBook-Air.local>
2024-07-14 19:39:08 -0400

    added mui dashboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import LoginPage from "./components/LoginPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainLayout from "./MainLayout";
+import Dashboard from "./dashboard/Dashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route index path="/" element={<LoginPage />} />
+          <Route index path="/dash" element={<Dashboard />} />
           <Route path="*" element={<MainLayout />} />
         </Routes>
       </Router>
