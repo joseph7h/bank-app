5735ba3fdc310db1ae42c6d47316c1fa34ac6b16
Jun An <joseph7h@gmail.com>
2024-07-17 10:42:46 -0400

    removed navbar.js

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,4 +1,3 @@
-import NavBar from "./components/Navbar";
 import ViewTransactions from "./components/ViewTransactions";
 import UserPage from "./components/UserPage";
 import { BrowserRouter as Router, Rou, Route, Routes } from "react-router-dom";
@@ -6,7 +5,6 @@ import { BrowserRouter as Router, Rou, Route, Routes } from "react-router-dom";
 function MainLayout() {
   return (
     <div className="App">
-      <NavBar />
       <Routes>
         <Route 
           exact 
